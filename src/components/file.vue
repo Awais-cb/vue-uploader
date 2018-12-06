091413bfe27ecb09f330972633f19a5f87a8266b
Awais-cb <awaisfiazit@gmail.com>
2018-12-06 17:38:34 +0500

    UI changes made

--- src/components/file.vue
+++ src/components/file.vue
@@ -1,43 +1,82 @@
 <template>
-          
-  <v-flex lg10 class="uploaderprogress__block">
-    <div class="inner__uploaderprogress">
-      <div class="uploader__progress my-2">
-        <v-toolbar  class="uploader_details py-1 px-1">
-          <div class="v-toolbar-title videothum">
-            <img src="../images/VID.png">
-          </div>
-          <v-spacer class="text my-1 mx-2">
-            <div class="body-2 uploader_title">
-              Tum Se Hi Full Song
-            </div>
-            <div class="progress">
-              <v-progress-linear color="success" height="2" value="90" ></v-progress-linear>
-            </div>
-            <div class="uploader_text">
-              <div class="caption">
-                120-k
-                <p class="notifation_success d-inline mx-2">
-                   - uploaded Successfully 30%
-                </p>
+  <div>
+       <slot
+      :file="file"
+      :list="list"
+      :status="status"
+      :paused="paused"
+      :error="error"
+      :average-speed="averageSpeed"
+      :formated-average-speed="formatedAverageSpeed"
+      :current-speed="currentSpeed"
+      :is-complete="isComplete"
+      :is-uploading="isUploading"
+      :size="size"
+      :formated-size="formatedSize"
+      :uploaded-size="uploadedSize"
+      :progress="progress"
+      :progress-style="progressStyle"
+      :progressing-class="progressingClass"
+      :time-remaining="timeRemaining"
+      :formated-time-remaining="formatedTimeRemaining"
+      :type="type"
+      :extension="extension"
+      :file-category="fileCategory"
+      >       
+      <!-- <v-flex lg10 class="uploaderprogress__block"> -->
+        <div class="inner__uploaderprogress">
+          <div class="uploader__progress my-2">
+            <v-toolbar  class="uploader_details py-1 px-1">
+              <div class="v-toolbar-title videothum">
+                <img src="../images/VID.png">
               </div>
-            </div>
-          </v-spacer>
-          <div class="v-toolbar-title btnholder">
-            <div class="text-xs-center">        
-              <v-btn round color="primary" dark @click="dialog = true">
-                <v-icon class="edit">create</v-icon>
-              </v-btn>
-              <v-btn round color="primary" dark>
-                <v-icon>delete_sweep</v-icon>
-              </v-btn>
-            </div>
+              <v-spacer class="text my-1 mx-2">
+                <div class="body-2 uploader_title">
+                  {{file.name}}
+                </div>
+                <div class="progress">
+                  <v-progress-linear color="success" height="2" :value="progressStyle.progress" ></v-progress-linear>
+                </div>
+                <div class="uploader_text">
+                  <template v-show="status !== 'uploading'">
+                      <div class="caption">
+                        <p class="notifation_success d-inline mx-2">
+                          {{statusText}}
+                        </p>
+                      </div>
+                  </template>
+                  <template  v-show="status === 'uploading'">
+                    <div class="caption">
+                        File size {{formatedSize}}
+                      <p class="notifation_success d-inline mx-2">
+                         - Progress {{progressStyle.progress}}
+                      </p>
+                      <p class="notifation_success d-inline mx-2">
+                         - Avarage speed {{formatedAverageSpeed}}
+                      </p>
+                      <p class="notifation_success d-inline mx-2">
+                         - Remaining time {{formatedTimeRemaining}}
+                      </p>
+                    </div>
+                  </template>
+                </div>
+              </v-spacer>
+              <div class="v-toolbar-title btnholder">
+                <div class="text-xs-center">        
+                  <v-btn round color="primary" dark @click="dialog = true">
+                    <v-icon class="edit">create</v-icon>
+                  </v-btn>
+                  <v-btn round color="primary" dark @click="remove">
+                    <v-icon>delete_sweep</v-icon>
+                  </v-btn>
+                </div>
+              </div>
+            </v-toolbar>
           </div>
-        </v-toolbar>
-      </div>
-    </div>
-  </v-flex>  
-
+        </div>
+    </slot>
+  <!-- </v-flex>  --> 
+  <!--
   <div class="uploader-file" :status="status">
     <slot
       :file="file"
@@ -84,6 +123,8 @@
       </div>
     </slot>
   </div>
+  -->
+</div>
 </template>
 
 <script>
